ffb9ac82f2d6b31c74a5c96add3e6724b71cfac0
thebearingedge <contact@timdav.is>
2019-04-05 16:11:41 -0700

    Copy the __env__ directory explicitly by name. #1

--- __env__/macos/install.bash
+++ __env__/macos/install.bash
@@ -4,7 +4,7 @@ setup_path="$desktop_path/student-dev-env"
 docker_compose_path="$lfz_path/__env__/docker/lfz/docker-compose.yml"
 local_bin="/Users/$(whoami)/.local/bin"
 
-cp -R $setup_path/__env__/ $lfz_path/ && \
+cp -R $setup_path/__env__ $lfz_path/__env__ && \
 cp $setup_path/*.php $lfz_path/ && \
 mkdir -p $local_bin && \
 echo "docker-compose -f $docker_compose_path up -d && docker-compose -f $docker_compose_path exec dev /bin/bash" > $local_bin/lfz-dev && \
