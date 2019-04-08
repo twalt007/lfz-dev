bcb9a53c4fc339e9e88ba68994be4013da18de3e
thebearingedge <contact@timdav.is>
2019-04-08 10:40:11 -0700

    Add preflight checks to install script. #1

--- __env__/macos/install.bash
+++ __env__/macos/install.bash
@@ -4,6 +4,20 @@ setup_path="$desktop_path/student-dev-env"
 docker_compose_path="$lfz_path/__env__/docker/lfz/docker-compose.yml"
 local_bin="/Users/$(whoami)/.local/bin"
 
+if [ ! -d $lfz_path ]; then
+  echo -e "\nNo lfz folder found on your Desktop!"
+  echo -e "Be sure to it exists before trying again."
+  echo -e "Aborting..."
+  exit 1
+fi
+
+command -v docker-compose >/dev/null 2>&1 || {
+  echo -e "\nCould not find the required Docker commands."
+  echo -e "Be sure to install Docker before trying again."
+  echo -e "Aborting..."
+  exit 1
+}
+
 cp -R $setup_path/__env__ $lfz_path/__env__ && \
 cp $setup_path/*.php $lfz_path/ && \
 mkdir -p $local_bin && \
---
+++ test.bash
@@ -0,0 +1,12 @@
+desktop_path="/Users/$(whoami)/Desktop"
+lfz_path="$desktop_path/lfz"
+setup_path="$desktop_path/student-dev-env"
+docker_compose_path="$lfz_path/__env__/docker/lfz/docker-compose.yml"
+local_bin="/Users/$(whoami)/.local/bin"
+
+if [ ! -d $lfz_path ]; then
+  echo -e "\nNo lfz folder found on your Desktop!"
+  echo -e "Be sure to it exists before trying again."
+  echo -e "Aborting..."
+  exit 1
+fi
